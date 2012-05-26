c13e29a3d998a1452adbb72a529e12183543708c
Viquoc Quan <chuon-chuon@hotmail.com>
2012-05-25 21:06:06 -0400

    Implement card draw function

--- ProbSimulator/ProbSimulator/cardPage.xaml.cs
+++ ProbSimulator/ProbSimulator/cardPage.xaml.cs
@@ -22,6 +22,85 @@ namespace ProbSimulator
 
         private void cardDrawB_Click(object sender, RoutedEventArgs e)
         {
+                cardDraw();
+        }
+
+        public void cardDraw()
+        {
+            Random random = new Random();
+
+            int result = 0;
+            String suit = "";
+            String value = "";
+            result = random.Next(0, 4);
+            switch (result)
+            {
+                case 1:
+                    suit = "Hearts";
+                    break;
+                case 2:
+                    suit = "Clubs";
+                    break;
+                case 3:
+                    suit = "Diamonds";
+                    break;
+                default:
+                    suit = "Spades";
+                    break;
+            }
+            result = random.Next(0, 13);
+            result++;
+            switch (result)
+            {
+                case 1:
+                    value = "Ace";
+                    break;
+                case 2:
+                    value = "2";
+                    break;
+                case 3:
+                    value = "3";
+                    break;
+                case 4:
+                    value = "4";
+                    break;
+                case 5:
+                    value = "5";
+                    break;
+                case 6:
+                    value = "6";
+                    break;
+                case 7:
+                    value = "7";
+                    break;
+                case 8:
+                    value = "8";
+                    break;
+                case 9:
+                    value = "9";
+                    break;
+                case 10:
+                    value = "10";
+                    break;
+                case 11:
+                    value = "Jack";
+                    break;
+                case 12:
+                    value = "Queen";
+                    break;
+                default:
+                    value = "King";
+                    break;
+            }
+            if (value.Equals("Ace") || value.Equals("8"))
+            {
+                resultBox.Text = "An " + value + " of " + suit;
+            }
+            else
+            {
+                resultBox.Text = "A " + value + " of " + suit;
+            }
+
 
         }
     }
