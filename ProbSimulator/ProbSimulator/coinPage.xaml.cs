eee3e1460ed773e8c3ecfefd29cc710b2603d1eb
Viquoc Quan <chuon-chuon@hotmail.com>
2012-10-15 12:13:26 -0300

    Fix wrong variable name

--- ProbSimulator/ProbSimulator/coinPage.xaml.cs
+++ ProbSimulator/ProbSimulator/coinPage.xaml.cs
@@ -85,7 +85,7 @@ namespace ProbSimulator
             numTailBox.Text = numTail.ToString();
 
             stackPanel1.Children.Add(new TextBlock() { Text = "  " });
-            historyScroll.UpdateLayout();
+            scrollViewer1.UpdateLayout();
             scrollViewer1.ScrollToVerticalOffset(stackPanel1.ActualHeight);
 
         }
